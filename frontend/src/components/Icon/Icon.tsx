64fca3f0924b6a7e3aba9dafbed99dd759fa321e
yoha6865 <yoha6865@gmail.com>
2024-10-20 22:23:26 +0900

    fe/feat: Icon 컴포넌트 구현
    #10

---
+++ frontend/src/components/Icon/Icon.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode } from 'react';
+import { IconColor } from './Icon.types';
+import theme from '../../styles/theme.css';
+import { icon } from './Icon.css';
+
+interface IconProps {
+  children: ReactNode;
+  size?: number;
+  color?: IconColor;
+  background?: IconColor | 'none';
+  rounded?: number;
+}
+
+const Icon = ({
+  children,
+  size = 1,
+  color = 'primary',
+  background = 'primary',
+  rounded = 1,
+  ...props
+}: IconProps) => {
+  return (
+    <div
+      className={icon}
+      style={{
+        padding: `${size}rem`,
+        color: theme.color.icon[color],
+        backgroundColor: background
+          ? background === 'none'
+            ? 'none'
+            : theme.color.iconbackground[background]
+          : theme.color.iconbackground[color],
+        borderRadius: `${rounded}rem`,
+      }}
+      {...props}
+    >
+      {React.cloneElement(children as React.ReactElement, {
+        style: { width: `${size}rem`, height: `${size}rem` },
+      })}
+    </div>
+  );
+};
+
+export default Icon;
