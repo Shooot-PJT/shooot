d64fe1cb629209b65429653fefd5080ddf34ef42
조성우 <he12569@gmail.com>
2024-10-21 16:47:36 +0900

    fe/feat: APIDocs 페이지 기본 폴더구조, API 마크업 기초 설계 중

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css';
 import Typography from './components/Typography';
+// import { API } from './pages/APIDocs/components/API/API';
 import darkTheme from './styles/darkTheme.css';
 
 function App() {
   return (
     <div className={darkTheme}>
+      {/* <API>
+        <API.Header
+          title={'특별한 계란 목록 가져오기'}
+          managerName={''}
+          method={'GET'}
+          needAuthorize={false}
+          endPoint={''}
+        />
+      </API> */}
       <Typography color="secondary" weight="700" size={1.5}>
         hello
       </Typography>
