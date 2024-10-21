ddd1a4df537ed4705f08d6a8a5bb9166ef03e6bc
조성우 <he12569@gmail.com>
2024-10-21 13:51:45 +0900

    fe/feat: Button 구현
    button 컴포넌트 구현
    - disabled, paddingX, paddingY, fullWidth, color, rounded 설정 가능
    
    #7

---
+++ frontend/src/components/Button/Button.tsx
@@ -0,0 +1,42 @@
+import { ReactNode } from 'react';
+import { ButtonColor } from './Button.types';
+import * as s from './Button.css';
+
+interface ButtonProps {
+  children: ReactNode;
+  color?: ButtonColor;
+  rounded: number;
+  fullWidth?: boolean;
+  paddingX: number;
+  paddingY: number;
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+const Button = ({
+  children = '버튼',
+  rounded = 0.75,
+  color = 'primary',
+  fullWidth,
+  paddingX = 1,
+  paddingY = 0.5,
+  disabled = false,
+  onClick,
+}: ButtonProps) => {
+  return (
+    <div
+      className={s.button({ color, fullWidth })}
+      style={{
+        borderRadius: `${rounded}rem`,
+        padding: `${paddingY}rem ${paddingX}rem`,
+        opacity: disabled ? 0.6 : 1,
+        cursor: disabled ? 'not-allowed' : 'pointer',
+      }}
+      onClick={disabled ? undefined : onClick}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Button;
