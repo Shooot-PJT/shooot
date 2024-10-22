e81aa67d8b2fcf10b0bed4220dda71d12c6ad78a
김현진 <khj745700@naver.com>
2024-10-23 01:55:18 +0900

    be/feat: api info extractor 기능 추가
    #6

---
+++ backend/shooot/src/main/java/com/shooot/application/projecttest/controller/ProjectBuildController.java
@@ -0,0 +1,13 @@
+package com.shooot.application.projecttest.controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/project/server")
+@RestController
+public class ProjectBuildController {
+
+}
