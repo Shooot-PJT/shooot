4df6e99218dda0dfab708970108b8c5f8ac49b88
최요하 <yoha6865@gmail.com>
2024-10-25 16:01:25 +0900

    fe/fix: Flexbox 수정
    특정 css 속성만 놔두고 모두 제거
    
    #12

--- frontend/src/components/Flexbox/Flexbox.css.ts
+++ frontend/src/components/Flexbox/Flexbox.css.ts
@@ -1,6 +1,32 @@
-import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 
-export const flexbox = style({
-  display: 'flex',
-  boxSizing: 'border-box',
+export const flexbox = recipe({
+  base: {
+    display: 'flex',
+    boxSizing: 'border-box',
+  },
+
+  variants: {
+    flexDirections: {
+      row: { flexDirection: 'row' },
+      'row-rev': { flexDirection: 'row-reverse' },
+      col: { flexDirection: 'column' },
+      'col-rev': { flexDirection: 'column-reverse' },
+    },
+    justifyContents: {
+      center: { justifyContent: 'center' },
+      between: { justifyContent: 'space-between' },
+      around: { justifyContent: 'space-around' },
+      evenly: { justifyContent: 'space-evenly' },
+      start: { justifyContent: 'start' },
+      end: { justifyContent: 'end' },
+      stretch: { justifyContent: 'stretch' },
+    },
+    alignItems: {
+      center: { alignItems: 'center' },
+      start: { alignItems: 'start' },
+      end: { alignItems: 'end' },
+      stretch: { alignItems: 'stretch' },
+    },
+  },
 });
