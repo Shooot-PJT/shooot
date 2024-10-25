9510e192d84bd40ff9baad45d9dc3bfcfc62fede
최요하 <yoha6865@gmail.com>
2024-10-25 16:42:53 +0900

    fe/feat: Banner 컴포넌트 구현
    #12

--- frontend/src/components/Banner/Banner.css.ts
+++ frontend/src/components/Banner/Banner.css.ts
@@ -1,13 +1,5 @@
 import { style } from '@vanilla-extract/css';
 
-export const container = style({
-  width: '100%',
-});
-
-export const edit = style({
-  textDecoration: 'underline',
-});
-
 export const imgdisplay = style({
   '@media': {
     'screen and (min-width: 1440px)': {
@@ -16,22 +8,6 @@ export const imgdisplay = style({
 
     'screen and (max-width: 1439px)': {
       display: 'none',
-    },
-  },
-});
-
-export const imgsize = style({
-  '@media': {
-    'screen and (min-width: 768px)': {
-      width: '10rem',
-    },
-
-    'screen and (min-width: 1024px)': {
-      width: '12rem',
-    },
-
-    'screen and (min-width: 1440px)': {
-      width: '20rem',
     },
   },
 });
