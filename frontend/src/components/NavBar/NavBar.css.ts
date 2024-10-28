ae27fee5f0abe899745d9e3db4c060af9db3dbb0
최요하 <yoha6865@gmail.com>
2024-10-28 09:30:47 +0900

    fe/feat: NavBar storybook 추가
    #12

--- frontend/src/components/NavBar/NavBar.css.ts
+++ frontend/src/components/NavBar/NavBar.css.ts
@@ -1,4 +1,5 @@
 import { style } from '@vanilla-extract/css';
+import theme from '../../styles/theme.css';
 
 export const container = style({
   boxSizing: 'border-box',
@@ -51,6 +52,39 @@ export const desktopS = style({
 
     'screen and (max-width: 1439px)': {
       display: 'block',
+    },
+  },
+});
+
+/* 스토리북에서 사용 */
+export const nav = style({
+  width: '100%',
+  display: 'flex',
+
+  '@media': {
+    'screen and (min-width: 1440px)': {
+      flexDirection: 'column',
+      alignItems: 'center',
+      rowGap: '1rem',
+    },
+
+    'screen and (max-width: 1439px)': {
+      display: 'flex',
+      alignItems: 'center',
+      marginTop: '3rem',
+      columnGap: '3rem',
+    },
+  },
+});
+
+export const divi = style({
+  width: '0.1875rem',
+  height: '7rem',
+  backgroundColor: theme.color.background['300'],
+
+  '@media': {
+    'screen and (min-width: 1440px)': {
+      display: 'none',
     },
   },
 });
