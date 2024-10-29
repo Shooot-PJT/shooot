1d8e4fc869aa86530ff87e721e9455721756d6b8
최요하 <yoha6865@gmail.com>
2024-10-29 17:57:51 +0900

    fe/feat: Banner - Contents 사용자 정보 조회 구현
    #25

--- frontend/src/components/Banner/Contents.tsx
+++ frontend/src/components/Banner/Contents.tsx
@@ -1,9 +1,34 @@
+import { useQueries } from '@tanstack/react-query';
 import { useNavBarStore } from '../../stores/navbarStore';
 import Flexbox from '../Flexbox';
 import Typography from '../Typography';
+import { getUserInfo } from '../../pages/Main/apis';
+import { useEffect } from 'react';
 
 export const Contents = () => {
   const navbarStore = useNavBarStore();
+  const [userInfo, projectInfo] = useQueries({
+    queries: [
+      {
+        queryKey: ['userinfo'],
+        queryFn: async () => await getUserInfo(),
+        enabled: false,
+      },
+      {
+        queryKey: ['projectinfo'],
+        queryFn: () => console.log('project info'),
+        enabled: false,
+      },
+    ],
+  });
+
+  useEffect(() => {
+    if (navbarStore.menu === 2) {
+      userInfo.refetch();
+    } else {
+      projectInfo.refetch();
+    }
+  }, [navbarStore.menu]);
 
   return (
     <Flexbox
@@ -21,14 +46,14 @@ export const Contents = () => {
           <Flexbox flexDirections="col" style={{ rowGap: '0.25rem' }}>
             <Flexbox alignItems="end" style={{ columnGap: '0.25rem' }}>
               <Typography color="secondary" size={1.25} weight="700">
-                닉네임입니다
+                {userInfo.data?.data.nickname}
               </Typography>
               <Typography color="light" weight="700" size={0.875}>
                 님 오늘도 와주셨군요!
               </Typography>
             </Flexbox>
             <Typography color="disabled" size={0.875} weight="500">
-              ssafy@ssafy.com
+              {userInfo.data?.data.email}
             </Typography>
           </Flexbox>
 
