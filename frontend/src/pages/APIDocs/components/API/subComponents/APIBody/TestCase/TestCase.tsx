67da273ab49c7dfc07f999d5f4a8e2925200c03a
horiz.d <he1236@ajou.ac.kr>
2024-10-31 11:08:05 +0900

    fe/feat: API Body 마크업 구현
    1. ExpectdResponse, TestCase, TestCaseTable( 종류별 추가 구현 필요 ) 서브 컴포넌트 마크업 구현
    2. TestLogBox 미완성

--- frontend/src/pages/APIDocs/components/API/subComponents/APIBody/TestCase/TestCase.tsx
+++ frontend/src/pages/APIDocs/components/API/subComponents/APIBody/TestCase/TestCase.tsx
@@ -1,28 +1,64 @@
-import { useState } from 'react';
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useState,
+} from 'react';
 import Typography from '../../../../../../../components/Typography';
-import themeCss from '../../../../../../../styles/theme.css';
 import { CollapseIcon } from '../../APICommon/CollapseIcon/CollapseIcon';
 import TestButton from '../../APICommon/TestButton/TestButton';
 import { ExpectedResponse } from './ExpectedResponse/ExpectedResponse';
 import { TestCaseTable } from './TestCaseTable/TestCaseTable';
 import Button from '../../../../../../../components/Button';
-import { testcaseDummyList } from '../../../../../dummies/testcase_dummy_list';
-import colorPalette from '../../../../../../../styles/colorPalette';
+import {
+  testcaseDummyList,
+  TestCaseHeaderInfo,
+} from '../../../../../dummies/testcase_dummy_list';
 import {
   CustomTab,
   CustomTabs,
 } from '../../../../../../../components/CustomTabs/CustomTabs';
 import Flexbox from '../../../../../../../components/Flexbox';
+import * as s from './TestCase.css';
+import TestResultTail from '../../APIHeader/TestResultTail/TestResultTail';
+import useIsFocusedHook, {
+  useIsFocusedHookReturnType,
+} from '../../../../../hooks/useIsFocusedHook';
+import { throttle } from 'lodash';
+import { HTTP_STATUS_CODES } from '../../../../../types/httpStatus';
 
 interface TestCaseProps {
   children: React.ReactNode;
+  testCaseHeaderInfo: TestCaseHeaderInfo;
 }
 
-export const TestCase = ({ children }: TestCaseProps) => {
+interface TestCaseContextProps extends TestCaseProps {
+  useIsFocusedHook: useIsFocusedHookReturnType;
+}
+
+const TestCaseContext = createContext<TestCaseContextProps | null>(null);
+
+const useTestCaseContext = () => {
+  const context = useContext(TestCaseContext);
+  if (!context) {
+    throw new Error('');
+  }
+  return context;
+};
+
+interface TestCaseListProps {
+  // apiId: APIProps['id'];
+  children: ReactNode;
+}
+
+export const TestCaseList = ({
+  // apiId,
+  children,
+}: TestCaseListProps) => {
   return (
     <Flexbox
       flexDirections="col"
-      alignItems="normal"
       style={{
         width: '100%',
       }}
@@ -32,19 +68,52 @@ export const TestCase = ({ children }: TestCaseProps) => {
   );
 };
 
-TestCase.Header = function Header() {
+export const TestCase = ({ children, testCaseHeaderInfo }: TestCaseProps) => {
   return (
-    <Flexbox
-      flexDirections="row"
-      justifyContents="between"
-      style={{
-        padding: '0rem 1.5rem 0rem 0rem',
-        backgroundColor: themeCss.color.background[300],
-        border: '0.05rem solid',
-        borderColor: colorPalette.util[400],
-        borderRadius: '0.6rem',
-        overflow: 'hidden',
+    <TestCaseContext.Provider
+      value={{
+        children,
+        testCaseHeaderInfo,
+        useIsFocusedHook: {
+          ...useIsFocusedHook(),
+        },
       }}
+    >
+      <Flexbox
+        flexDirections="col"
+        alignItems="normal"
+        style={{
+          width: '100%',
+        }}
+      >
+        {children}
+      </Flexbox>
+    </TestCaseContext.Provider>
+  );
+};
+
+TestCase.Header = function Header() {
+  const context = useTestCaseContext();
+  const { isFocused, handleToggleIsFocused } = context.useIsFocusedHook;
+
+  const onClickHeader = useCallback(
+    throttle((e: React.MouseEvent) => {
+      e.stopPropagation();
+      handleToggleIsFocused();
+    }, 500),
+    [isFocused],
+  );
+
+  const statusCodeColor = context.testCaseHeaderInfo.statusCode
+    .charAt(0)
+    .match('2')
+    ? 'originalGreen'
+    : 'originalRed';
+
+  return (
+    <div
+      className={s.headerContainerRecipe({ isOpen: isFocused })}
+      onClick={onClickHeader}
     >
       <Flexbox
         flexDirections="row"
@@ -54,19 +123,11 @@ TestCase.Header = function Header() {
           height: '2.5rem',
         }}
       >
-        <div
-          style={{
-            height: '100%',
-            width: '0.6rem',
-            alignItems: 'center',
-            backgroundColor: 'red',
-          }}
-        />
-        <Typography size={0.85} weight="500" color={'originalRed'}>
-          401 (Unauthorized)
+        <Typography size={0.85} weight="500" color={statusCodeColor}>
+          {`${context.testCaseHeaderInfo.statusCode}  ${HTTP_STATUS_CODES[context.testCaseHeaderInfo.statusCode]}`}
         </Typography>
         <Typography size={0.8} weight="400" color={'disabled'}>
-          비인가 테스트용입니다. 슈웃 해주세요.
+          {context.testCaseHeaderInfo.description}
         </Typography>
       </Flexbox>
       <Flexbox
@@ -78,14 +139,18 @@ TestCase.Header = function Header() {
       >
         <TestButton.API />
         <CollapseIcon isOpen={false} />
+        <TestResultTail lastTestResult="success" />
       </Flexbox>
-    </Flexbox>
+    </div>
   );
 };
 
 TestCase.Body = function Body() {
   const [tabValue, setTabValue] = useState(0);
   const [isEditing, setIsEditing] = useState(false);
+
+  const context = useTestCaseContext();
+  const { isFocused } = context.useIsFocusedHook;
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setTabValue(newValue);
@@ -96,20 +161,7 @@ TestCase.Body = function Body() {
   };
 
   return (
-    <Flexbox
-      flexDirections="col"
-      alignItems="normal"
-      style={{
-        gap: '4rem',
-        borderLeft: '0.07rem solid',
-        borderRight: '0.07rem solid',
-        borderBottom: '0.07rem solid',
-        borderRadius: '0rem 0rem 0.5rem 0.5rem',
-        borderColor: colorPalette.util[400],
-        backgroundColor: themeCss.color.background[200],
-        padding: '0.75rem',
-      }}
-    >
+    <div className={s.testBodyContainerRecipe({ isOpen: isFocused })}>
       {/* 1. 테이블 섹션 */}
       <Flexbox
         flexDirections="col"
@@ -137,7 +189,7 @@ TestCase.Body = function Body() {
         </Flexbox>
 
         {/* 1.2 Tabs 컨텐츠: TestCaseTable */}
-        <TestCaseTable>
+        <TestCaseTable isEditing={isEditing}>
           <TestCaseTable.Section
             headers={testcaseDummyList[tabValue].headers}
             rows={testcaseDummyList[tabValue].rows}
@@ -147,10 +199,10 @@ TestCase.Body = function Body() {
       </Flexbox>
 
       {/* Expected Responses */}
-      <ExpectedResponse>
+      <ExpectedResponse isEditing={isEditing}>
         <ExpectedResponse.Schema />
         <ExpectedResponse.Example />
       </ExpectedResponse>
-    </Flexbox>
+    </div>
   );
 };
