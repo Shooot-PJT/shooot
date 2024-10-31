1d5c2e0f36fd8a61532bd365dfda5c02a94fc8a7
최요하 <yoha6865@gmail.com>
2024-10-31 16:55:20 +0900

    fe/feat: 내프로젝트 조회 구현
    #25

--- frontend/src/pages/Main/index.tsx
+++ frontend/src/pages/Main/index.tsx
@@ -1,6 +1,7 @@
 import { Layout } from '../../components/Layout';
 import { useNavBarStore } from '../../stores/navbarStore';
 import { APIDocs } from '../APIDocs';
+import { MyProject } from '../MyProject';
 
 export const Main = () => {
   const navbarStore = useNavBarStore();
@@ -9,7 +10,7 @@ export const Main = () => {
     <Layout>
       {navbarStore.menu === 0 && <APIDocs />}
       {navbarStore.menu === 1 && <>test</>}
-      {navbarStore.menu === 2 && <>project</>}
+      {navbarStore.menu === 2 && <MyProject />}
     </Layout>
   );
 };
