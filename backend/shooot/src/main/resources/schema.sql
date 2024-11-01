2a849373b3460ef26be339daecff16f8a74a12bb
박민희 <minuteman0212@naver.com>
2024-11-01 16:27:01 +0900

    be/fix: 프로젝트 생성 기능 요청 값 검증
    #32

--- backend/shooot/src/main/resources/schema.sql
+++ backend/shooot/src/main/resources/schema.sql
@@ -29,10 +29,10 @@ CREATE TABLE project
 (
     project_id   INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name         VARCHAR(80)         NOT NULL,
-    english_name VARCHAR(10)         NOT NULL,
+    english_name VARCHAR(20)         NOT NULL,
     content_type VARCHAR(30),
     filename     VARCHAR(100),
-    memo         TEXT                NOT NULL,
+    memo         TEXT,
     is_deleted   BOOL                NOT NULL,
     created_at   DATETIME            NOT NULL
 );
