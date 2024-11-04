03be8da61dadfe04f9c3de540404ce3e325b0c8d
박민희 <minuteman0212@naver.com>
2024-11-04 10:16:01 +0900

    be/chore: 프로젝트 생성 기능 예외 처리 Validation 사용하지 않도록 변경
    #34

--- backend/shooot/src/main/java/com/shooot/application/project/service/dto/ProjectRegisterRequest.java
+++ backend/shooot/src/main/java/com/shooot/application/project/service/dto/ProjectRegisterRequest.java
@@ -1,8 +1,5 @@
 package com.shooot.application.project.service.dto;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,12 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProjectRegisterRequest {
 
-    @NotNull
     private String name;
-
-    @Size(min = 1, max = 20, message = "사용자 이름은 1자 이상 20자 이하여야 합니다.")
-    @Pattern(regexp = "^[a-z0-9-]+$", message = "사용자 이름은 영어 소문자, 숫자, 그리고 하이픈(-)만 포함할 수 있습니다.")
     private String englishName;
-
     private String memo;
 }
