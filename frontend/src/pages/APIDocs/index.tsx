48924156d5522e76d036770a71f22523656dc2f4
조성우 <he12569@gmail.com>
2024-11-04 16:54:13 +0900

    fe/feat: 도메인 더미데이터 추가, APIDocs 페이지 컴포넌트에 조합
    #30

--- frontend/src/pages/APIDocs/index.tsx
+++ frontend/src/pages/APIDocs/index.tsx
@@ -1,27 +1,33 @@
 import Flexbox from '../../components/Flexbox';
 import { API } from './components/API/API';
+import { AuthorizeButton } from './components/AuthorizeButton/AuthorizeButton';
+import { Domain } from './components/Domain/Domain';
 import { DUMMY_API_HEADER_INFO_LIST } from './dummies/api_header_info_list';
+import { DOMAIN_INFO_LIST_DUMMY } from './dummies/domain_list_dummy';
 
 export const APIDocs = () => {
   return (
     <Flexbox
-      alignItems="center"
       flexDirections="col"
-      justifyContents="start"
       style={{
-        gap: '0.9rem',
-        width: '100%',
-        height: '100%',
+        gap: '1rem',
+        padding: '2rem',
       }}
     >
-      {DUMMY_API_HEADER_INFO_LIST.map((header_info) => {
-        return (
-          <API key={header_info.apiId} header_info={header_info}>
-            <API.Header />
-            <API.Body />
-          </API>
-        );
-      })}
+      <AuthorizeButton isAuthorized={true} />
+      {DOMAIN_INFO_LIST_DUMMY.map((domainInfo) => (
+        <Domain key={domainInfo.domainId} domainInfo={domainInfo}>
+          <Domain.Header />
+          <Domain.Body>
+            {DUMMY_API_HEADER_INFO_LIST.map((headerInfo) => (
+              <API key={headerInfo.apiId} headerInfo={headerInfo}>
+                <API.Header />
+                <API.Body />
+              </API>
+            ))}
+          </Domain.Body>
+        </Domain>
+      ))}
     </Flexbox>
   );
 };
