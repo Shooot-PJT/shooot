d0425bda32f272adeb539f68f420a3c0c7ba2812
horiz.d <he1236@ajou.ac.kr>
2024-11-04 16:53:14 +0900

    fe/feat: 도메인 컴포넌트 마크업 구현
    1. 도메인 헤더 마크업 구현
    2. 도메인 바디 마크업 구현
    3. 도메인 컴파운드 및 Context 설계
    
    #30

---
+++ frontend/src/pages/APIDocs/components/Domain/Body/Body.css.ts
@@ -0,0 +1,7 @@
+export { CollapseContainerRecipe } from '../../../index.css';
+
+export const BodyContainerStyle = {
+  gap: '0.9rem',
+  width: '100%',
+  height: '100%',
+};
--- frontend/src/pages/APIDocs/components/Domain/Domain.data.types.ts
+++ frontend/src/pages/APIDocs/components/Domain/Domain.data.types.ts
@@ -1,8 +1,9 @@
-export interface Domain {
+export type isSubscribed = boolean;
+
+export interface DomainInfo {
   domainId: number;
   projectId: number;
   title: string;
   description: string;
+  isSubscribed: isSubscribed; // 요청자의 알림 구독여부
 }
-
-export type DomainList = Domain[];
