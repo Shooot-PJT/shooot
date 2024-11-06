24d81c91a00b9f996b04eb6d58e58f599df9617a
altys31 <altys31@gmail.com>
2024-11-06 18:00:25 +0900

    fe/wip: 프로젝트 추가 api 연결중

---
+++ frontend/src/pages/ServerTest/components/UploadingModal/UploadingModal.tsx
@@ -0,0 +1,41 @@
+import Lottie from 'lottie-react';
+import rocket from '../../../../assets/Rocket.json';
+import Flexbox from '../../../../components/Flexbox';
+import Typography from '../../../../components/Typography';
+import * as s from './UploadingModal.css';
+export interface UploadingModalProps {
+  panding: boolean;
+}
+
+export const UploadingModal = () => {
+  return (
+    <Flexbox flexDirections="col" justifyContents="center" alignItems="center">
+      <Typography size={1.5} weight="600">
+        업로드 중 입니다...
+      </Typography>
+      <Flexbox
+        justifyContents="center"
+        alignItems="center"
+        style={{
+          width: '100%',
+          position: 'relative',
+          overflow: 'hidden',
+        }}
+      >
+        <div className={s.moon}>
+          <div className={s.pattern1} />
+          <div className={s.pattern2} />
+          <div className={s.pattern3} />
+        </div>
+        <div
+          style={{
+            width: '150px',
+            height: '200px',
+          }}
+        >
+          <Lottie animationData={rocket} />
+        </div>
+      </Flexbox>
+    </Flexbox>
+  );
+};
