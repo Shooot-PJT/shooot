4d9a74af0097308f351a91a29629b41e543c1a0b
altys31 <altys31@gmail.com>
2024-11-07 21:30:09 +0900

    fe/feat: 기능 타이틀 추가

--- frontend/src/pages/ServerTest/components/ProjectTable/ProjectTable.tsx
+++ frontend/src/pages/ServerTest/components/ProjectTable/ProjectTable.tsx
@@ -6,6 +6,7 @@ import { AddProjectModal } from '../AddProjectModal/AddProjectModal';
 import { DataTable } from '../DataTable/DataTable';
 import { TestConfigModal } from '../TestConfigModal/TestConfigModal';
 import { deleteJarFile } from '../../apis';
+import Typography from '../../../../components/Typography';
 
 interface ProjectProps {
   tableData: ReactNode[][];
@@ -50,25 +51,40 @@ export const ProjectTable = ({
   };
 
   return (
-    <>
+    <div style={{ width: '100%' }}>
       <Flexbox
         flexDirections="row"
-        justifyContents="end"
-        style={{ gap: '1rem', marginBottom: '0.5rem', marginRight: '0.5rem' }}
+        justifyContents="between"
+        alignItems="center"
+        style={{ marginBottom: '0.5rem' }}
       >
-        <Button rounded={0.5} color="tertiary" onClick={handleAddProjectModal}>
-          프로젝트 추가
-        </Button>
-        <Button
-          rounded={0.5}
-          color="delete"
-          onClick={handleDeleteProjectJarFile}
+        <Typography size={1.5} weight="600">
+          프로젝트 빌드파일
+        </Typography>
+
+        <Flexbox
+          flexDirections="row"
+          justifyContents="between"
+          style={{ gap: '1.5rem', marginBottom: '1rem' }}
         >
-          프로젝트 제거
-        </Button>
-        <Button rounded={0.5} onClick={handleTestConfigModal}>
-          테스트 실행
-        </Button>
+          <Button
+            rounded={0.5}
+            color="tertiary"
+            onClick={handleAddProjectModal}
+          >
+            프로젝트 추가
+          </Button>
+          <Button
+            rounded={0.5}
+            color="delete"
+            onClick={handleDeleteProjectJarFile}
+          >
+            프로젝트 제거
+          </Button>
+          <Button rounded={0.5} onClick={handleTestConfigModal}>
+            테스트 실행
+          </Button>
+        </Flexbox>
       </Flexbox>
       <DataTable
         colWidths={colWidths}
@@ -78,6 +94,6 @@ export const ProjectTable = ({
         selectedRowIndex={selectedRow}
         handleSelectRow={handleSelectRow}
       />
-    </>
+    </div>
   );
 };
