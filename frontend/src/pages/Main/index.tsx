0cd6738156bf76e479f77d6f14df4e666dbb43fa
최요하 <yoha6865@gmail.com>
2024-11-07 09:53:45 +0900

    fe/fix: 라우터 조정
    #47

--- frontend/src/pages/Main/index.tsx
+++ frontend/src/pages/Main/index.tsx
@@ -2,6 +2,7 @@ import { Layout } from '../../components/Layout';
 import { useNavBarStore } from '../../stores/navbarStore';
 import { APIDocs } from '../APIDocs';
 import { MyProject } from '../MyProject';
+import { ServerTest } from '../ServerTest';
 
 export const Main = () => {
   const navbarStore = useNavBarStore();
@@ -9,7 +10,7 @@ export const Main = () => {
   return (
     <Layout>
       {navbarStore.menu === 0 && <APIDocs />}
-      {navbarStore.menu === 1 && <>test</>}
+      {navbarStore.menu === 1 && <ServerTest />}
       {navbarStore.menu === 2 && <MyProject />}
     </Layout>
   );
