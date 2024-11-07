1ea2b00cce70adf4b5fee5333acde008977abba2
최요하 <yoha6865@gmail.com>
2024-11-07 09:46:54 +0900

    fe/feat: 로그아웃 구현
    #40

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -6,6 +6,12 @@ import { Banner } from '../Banner';
 import { ErrorBoundary } from 'react-error-boundary';
 import { useResize } from '../../hooks/useResize';
 import { useNavBar } from '../../pages/Main/hooks/useNavBar';
+import Button from '../Button';
+import Typography from '../Typography';
+import { useQueryClient } from '@tanstack/react-query';
+import { logout } from '../../pages/Main/apis';
+import { useNavigate } from 'react-router-dom';
+import usePopup from '../../hooks/usePopup';
 
 interface LayoutProps extends React.ComponentProps<'div'> {
   children: ReactNode;
@@ -24,6 +30,35 @@ export const Layout = ({ children, ...props }: LayoutProps) => {
     inviteMembersModalHandler,
     kickMemberModalHandler,
   } = useNavBar();
+  const queryClient = useQueryClient();
+  const nav = useNavigate();
+  const popup = usePopup();
+
+  const handleLogout = async () => {
+    await logout()
+      .then(() => {
+        queryClient.clear();
+        popup.push({
+          title: '로그아웃',
+          children: (
+            <Typography>
+              로그아웃 되었습니다.
+              <br />
+              로그인 페이지로 이동합니다.
+            </Typography>
+          ),
+          onClose: () => nav('/auth/login'),
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        popup.push({
+          title: '로그아웃 실패',
+          children: <Typography>다시 시도해주세요.</Typography>,
+          type: 'fail',
+        });
+      });
+  };
 
   return (
     <div className={style.layout} {...props}>
@@ -37,6 +72,18 @@ export const Layout = ({ children, ...props }: LayoutProps) => {
           </ErrorBoundary>
           <div className={style.divi} />
           <NavBar.Menu />
+        </div>
+        <div style={{ width: '90%', margin: '5rem auto -2rem auto' }}>
+          <Button
+            color="grey"
+            fullWidth
+            paddingY={0.75}
+            onClick={async () => await handleLogout()}
+          >
+            <Typography color="disabled" size={0.8125}>
+              로그아웃
+            </Typography>
+          </Button>
         </div>
       </NavBar>
       <div style={{ width: '100%' }}>
