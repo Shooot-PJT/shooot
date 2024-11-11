ba675cfbd3a4849f2edf70031c3d9fe959b16e2d
최요하 <yoha6865@gmail.com>
2024-11-11 17:31:18 +0900

    mock/feat: request 기반의 test case 식별
    #60

--- shooot-express/server.js
+++ shooot-express/server.js
@@ -62,20 +62,77 @@ app.get("/projects/search", (req, res) => {
 });
 
 // service worker 로부터 받은 정보
-app.post("/mock/data", (req, res) => {
-    const { projectName, url, origin, method, headers, requestParameters, pathVariables, body } = req.body;
+app.post("/mock/data", async (req, res) => {
+    const { projectName, url, origin, endpoint, method, headers, contentType, requestParameters, pathVariables, body } =
+        req.body;
 
     console.log("[Received data from Service Worker]");
     console.log("projectName:", projectName);
     console.log("url:", url);
     console.log("origin:", origin);
+    console.log("endpoint:", endpoint);
     console.log("method:", method);
     console.log("headers:", headers);
+    console.log("contentType:", contentType);
     console.log("requestParameters:", requestParameters);
     console.log("pathVariables:", pathVariables);
     console.log("body:", body);
 
-    res.json({ message: "데이터를 정상적으로 받았습니다" });
+    const apiQuery = `SELECT api_id FROM api WHERE method = "${method}" and url = "${endpoint}"`;
+    db.execute(apiQuery, (err, results) => {
+        if (err) {
+            console.error("[post, /mock/data, query1]:", err);
+            return res.status(500).json({ error: "Database query error" });
+        }
+
+        if (results.length) {
+            const testcaseQuery = `SELECT api_test_case_id FROM api_test_case WHERE api_id = "${results[0].api_id}"`;
+            db.execute(testcaseQuery, (err, results) => {
+                if (err) {
+                    console.error("[post, /mock/data, query2]:", err);
+                    return res.status(500).json({ error: "Database query error" });
+                }
+
+                if (results.length) {
+                    let newResults = [];
+                    results.forEach((result) => {
+                        const requestQuery = `SELECT * FROM api_test_case_request WHERE api_test_case_id = "${result.api_test_case_id}" ORDER BY api_test_request_id DESC LIMIT 1`;
+                        db.execute(requestQuery, (err, results) => {
+                            if (err) {
+                                console.error("[post, /mock/data, query2]:", err);
+                                return res.status(500).json({ error: "Database query error" });
+                            }
+
+                            console.log(results[0]);
+                            newResults.push(results[0]);
+                        });
+                    });
+                    return res.status(200).json(newResults);
+                } else {
+                    return res.status(404).json({ message: "등록된 api test case 가 없습니다" });
+                }
+            });
+        } else {
+            return res.status(404).json({ message: "등록된 api 가 없습니다" });
+        }
+    });
+
+    // const query = `SELECT api_test_case_id FROM api_test_case WHERE api_id = (SELECT api_id FROM api WHERE method = "${method}" and url = "${endpoint}")`;
+    // const query3 = `SELECT content FROM api_test_case_request WHERE api_test_case_id in (SELECT api_test_case_id FROM api_test_case WHERE api_id = (SELECT api_id FROM api WHERE method = "${method}" and url = "${endpoint}"))`;
+    // db.execute(query, (err, results) => {
+    //     if (err) {
+    //         console.error("[post, /mock/data]:", err);
+    //         return res.status(500).json({ error: "Database query error" });
+    //     }
+
+    //     if (results.length) {
+    //         return res.status(200).json(results[0]);
+    //     } else {
+    //         return res.status(404).json({ message: "NO API" });
+    //     }
+    // });
+
+    // return res.status(200).json(null);
 });
 
 // 서버 시작
