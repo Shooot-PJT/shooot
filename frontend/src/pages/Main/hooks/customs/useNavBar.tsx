67ef17c8f50864d6500abc3203c383fcb76eb374
최요하 <yoha6865@gmail.com>
2024-11-12 12:15:44 +0900

    fe/fix: 쿼리 최적화
    tanstack query 를 사용한 로직과 뷰를 분리
    - query, mutation 으로 구분지어 훅으로 분리
    - 필요한 곳에서 사용
    - stale time 을 명확히 정하고 필요한 곳에서는 새로 호출보다 invalidQueries 활용
    
    #59

---
+++ frontend/src/pages/Main/hooks/customs/useNavBar.tsx
@@ -0,0 +1,89 @@
+import { useQueryClient } from '@tanstack/react-query';
+import { useNavBarStore } from '../../../../stores/navbarStore';
+import { useEffect } from 'react';
+import useModal from '../../../../hooks/useModal';
+import { NicknameChangePopup } from '../../popups/Banner/NicknameChangeModal';
+import { ProjectWriteModal } from '../../popups/Banner/ProjectWriteModal/ProjectWriteModal';
+import { InviteMembersModal } from '../../popups/Banner/InviteMembersModal/InviteMembersModal';
+import { KickMemberModal } from '../../popups/Banner/KickMemberModal/KickMemberModal';
+import { ProjectRemoveModal } from '../../popups/Banner/ProjectRemoveModal';
+import { QUERY_KEYS } from '../../utils/KEYS';
+export const useNavBar = () => {
+  const navbarStore = useNavBarStore();
+  const modal = useModal();
+  const queryClient = useQueryClient();
+
+  // 닉네임 수정
+  const nicknameChangeModalHandler = () => {
+    modal.push({
+      children: <NicknameChangePopup />,
+    });
+  };
+
+  // 프로젝트 생성
+  const addProjectModalHandler = () => {
+    modal.push({
+      children: <ProjectWriteModal />,
+    });
+  };
+
+  // 프로젝트 수정
+  const editProjectModalHandler = () => {
+    modal.push({
+      children: <ProjectWriteModal type="edit" />,
+    });
+  };
+
+  // 팀원 초대
+  const inviteMembersModalHandler = () => {
+    modal.push({
+      children: <InviteMembersModal />,
+    });
+  };
+
+  // 팀원 추방
+  const kickMemberModalHandler = () => {
+    modal.push({
+      children: <KickMemberModal />,
+      onClose: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.readProjectList],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.readProjectByProjectId, navbarStore.project],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.readMembersByProjectId, navbarStore.project],
+        });
+      },
+    });
+  };
+
+  // 프로젝트 삭제
+  const removeProjectModalHandler = () => {
+    modal.push({
+      children: <ProjectRemoveModal />,
+    });
+  };
+
+  // useEffect(() => {
+  //   if (navbarStore.menu !== 2) {
+  //     queryClient.invalidateQueries({
+  //       queryKey: [QUERY_KEYS.readProjectByProjectId, navbarStore.project],
+  //     });
+  //     queryClient.invalidateQueries({
+  //       queryKey: [QUERY_KEYS.readMembersByProjectId, navbarStore.project],
+  //     });
+  //   }
+  // }, [navbarStore.menu, navbarStore.project]);
+
+  return {
+    menu: navbarStore.menu,
+    nicknameChangeModalHandler,
+    addProjectModalHandler,
+    editProjectModalHandler,
+    inviteMembersModalHandler,
+    kickMemberModalHandler,
+    removeProjectModalHandler,
+  };
+};
