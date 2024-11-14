6e4f50aa2a687d6a1947e300b4eb92b8ab97f6d3
박민희 <minuteman0212@naver.com>
2024-11-14 23:37:04 +0900

    be/feat: SSE 활용 부하 테스트 정보 전송 기능 구현

--- backend/shooot/src/main/java/com/shooot/application/projecttest/event/handler/ProjectStressTestHandler.java
+++ backend/shooot/src/main/java/com/shooot/application/projecttest/event/handler/ProjectStressTestHandler.java
@@ -3,7 +3,11 @@ package com.shooot.application.projecttest.event.handler;
 import com.shooot.application.api.domain.Api;
 import com.shooot.application.api.domain.repository.ApiRepository;
 import com.shooot.application.api.exception.api.ApiNotFoundException;
+import com.shooot.application.projecttest.domain.ApiTestMethod;
+import com.shooot.application.projecttest.domain.repository.ApiTestMethodRepository;
 import com.shooot.application.projecttest.event.dto.ProjectTestRequestedEvent;
+import com.shooot.application.sseemitter.service.StressTestSseService;
+import com.shooot.application.stresstest.controller.dto.StressTestDto;
 import com.shooot.application.stresstest.service.StressTestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
@@ -18,6 +22,8 @@ public class ProjectStressTestHandler {
 
     private final ApiRepository apiRepository;
     private final StressTestService stressTestService;
+    private final ApiTestMethodRepository apiTestMethodRepository;
+    private final StressTestSseService stressTestSseService;
 
     @Async
     @EventListener
@@ -25,7 +31,29 @@ public class ProjectStressTestHandler {
     public void stressTest(ProjectTestRequestedEvent event) {
         event.getApiId().forEach(apiId -> {
             Api api = apiRepository.findById(apiId).orElseThrow(ApiNotFoundException::new);
-            stressTestService.
+            ApiTestMethod apiTestMethod = apiTestMethodRepository.findByApi(api).orElseThrow();
+
+            switch (apiTestMethod.getBuildFileTestMethod()) {
+                case FIXED -> stressTestService.fixed(api, apiTestMethod.getVUsers(),
+                    apiTestMethod.getTestDuration());
+                case SPIKE -> stressTestService.spike(api, apiTestMethod.getVUsers(),
+                    apiTestMethod.getTestDuration());
+                case RAMP_UP -> stressTestService.rampUp(api, apiTestMethod.getVUsers(),
+                    apiTestMethod.getTestDuration());
+            }
+
+            long currentTime = System.currentTimeMillis();
+            long endTime = currentTime + apiTestMethod.getTestDuration() * 1000;
+
+            while (System.currentTimeMillis() < endTime) {
+                StressTestDto data = stressTestService.getData(event.getProjectJarFileId());
+                stressTestSseService.send(event.getProjectJarFileId(), data);
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         });
     }
 }
