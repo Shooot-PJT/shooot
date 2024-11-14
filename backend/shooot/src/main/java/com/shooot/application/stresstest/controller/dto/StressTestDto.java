a52e43a4ba3ffdf8d4a0e4c89fc4c4adfbce739c
박민희 <minuteman0212@naver.com>
2024-11-14 23:36:36 +0900

    be/feat: CPU 사용률, 메모리 사용률 측정 기능 구현

--- backend/shooot/src/main/java/com/shooot/application/stresstest/controller/dto/StressTestDto.java
+++ backend/shooot/src/main/java/com/shooot/application/stresstest/controller/dto/StressTestDto.java
@@ -1,19 +1,10 @@
 package com.shooot.application.stresstest.controller.dto;
 
+import lombok.Builder;
+
+@Builder
 public class StressTestDto {
 
     private Float cpuUtilization;
-    private Float ramUtilization;
-    private Float networkUtilization;
-    private Float diskUtilization;
-    private Float jvmMemoryUsage;
-    private Float gcPauseTime;
-    private Short threadCount;
-    private Long requestCount;
-    private Float heapMemoryUsage;
-    private Float nonHeapMemoryUsage;
-    private Long cacheHits;
-    private Long cacheMisses;
-    private Short activeConnections;
-    private Short totalConnections;
+    private Long ramUtilization;
 }
--- backend/shooot/src/main/java/com/shooot/application/stresstest/service/StressTestService.java
+++ backend/shooot/src/main/java/com/shooot/application/stresstest/service/StressTestService.java
@@ -1,31 +1,36 @@
 package com.shooot.application.stresstest.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shooot.application.api.domain.Api;
+import com.shooot.application.projecttest.domain.ProjectBuild;
+import com.shooot.application.projecttest.domain.repository.ProjectBuildRepository;
+import com.shooot.application.projecttest.exception.ProjectBuildNotFoundException;
+import com.shooot.application.sseemitter.service.StressTestSseService;
+import com.shooot.application.stresstest.controller.dto.StressTestDto;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicIntegerArray;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class StressTestService {
 
-    private final WebClient webClient;
-    private AtomicIntegerArray array;
-    private Api api = Api.builder()
-        .method("GET")
-        .url("https://pichu.shooot.shop/hello")
-        .build();
+    private final ProjectBuildRepository projectBuildRepository;
+    private final StressTestSseService stressTestSseService;
 
-    public void fixed(Integer vUser, Integer duration) {
+    public void fixed(Api api, Integer vUser, Integer duration) {
         long currentTime = System.currentTimeMillis();
         long endTime = currentTime + duration * 1000;
 
@@ -37,7 +42,7 @@ public class StressTestService {
         }
     }
 
-    public void spike(Integer vUser, Integer duration) {
+    public void spike(Api api, Integer vUser, Integer duration) {
         long currentTime = System.currentTimeMillis();
         long endTime = currentTime + duration * 1000;
         long interval = duration * 1000 / 2;
@@ -52,7 +57,7 @@ public class StressTestService {
         }
     }
 
-    public void rampUp(Integer vUser, Integer duration) {
+    public void rampUp(Api api, Integer vUser, Integer duration) {
         long currentTime = System.currentTimeMillis();
         long endTime = currentTime + duration * 1000;
         long interval = duration * 1000 / vUser;
@@ -62,6 +67,77 @@ public class StressTestService {
 
         for (int i = 0; i < vUser; i++) {
             executor.schedule(sender, interval * i, TimeUnit.MILLISECONDS);
+        }
+    }
+
+    @Transactional
+    public StressTestDto getData(Integer projectJarFileId) {
+        ProjectBuild projectBuild = projectBuildRepository.findById(projectJarFileId).orElseThrow(
+            ProjectBuildNotFoundException::new);
+
+        return StressTestDto.builder()
+            .cpuUtilization(getCpuUtilization(projectBuild.getProject().getEnglishName()))
+            .ramUtilization(getRamUtilization(projectBuild.getProject().getEnglishName()))
+            .build();
+    }
+
+    private float getCpuUtilization(String subDomain) {
+        try {
+            URL url = new URL(
+                "https://%s.shooot.shop/actuator/metrics/process.cpu.usage".formatted(subDomain));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/json");
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, Object> responseMap = objectMapper.readValue(content.toString(),
+                Map.class);
+            List<Map<String, Object>> measurements = (List<Map<String, Object>>) responseMap.get(
+                "measurements");
+            float value = (float) measurements.get(0).get("value");
+            connection.disconnect();
+            return value;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private long getRamUtilization(String subDomain) {
+        try {
+            URL url = new URL(
+                "https://%s.shooot.shop/actuator/metrics/jvm.memory.used".formatted(subDomain));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/json");
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, Object> responseMap = objectMapper.readValue(content.toString(),
+                Map.class);
+            List<Map<String, Object>> measurements = (List<Map<String, Object>>) responseMap.get(
+                "measurements");
+            long value = (long) measurements.get(0).get("value");
+            connection.disconnect();
+            return value;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
