5516fbaeb8778bd1b0cb24753669cc627470882b
조성우 <he12569@gmail.com>
2024-11-17 00:40:30 +0900

    fe/feat: expectedResponse ( schema, example ) 구현
    #79

--- frontend/src/pages/APIDocs/components/API/subComponents/APIBody/TestCase/ExpectedResponse/ExpectedResponse.css.ts
+++ frontend/src/pages/APIDocs/components/API/subComponents/APIBody/TestCase/ExpectedResponse/ExpectedResponse.css.ts
@@ -1,30 +1,30 @@
-import { style } from '@vanilla-extract/css';
+// import { style } from '@vanilla-extract/css';
 
-import colorPalette from '../../../../../../../../styles/colorPalette';
+// import colorPalette from '../../../../../../../../styles/colorPalette';
 
-// export { roundedBorder };
+// // export { roundedBorder };
 
-export const containerStyle = {
-  gap: '0.5rem',
-};
+// export const containerStyle = {
+//   gap: '0.5rem',
+// };
 
-const expectedResponseCommon = {
-  padding: '1rem',
-  borderRadius: '0.5rem',
-  backgroundColor: colorPalette.util[300],
-};
-export const addButton = style({
-  ...expectedResponseCommon,
-  cursor: 'pointer',
-  transition: 'all 0.25s ease-in-out',
-  selectors: {
-    '&:hover': {
-      backgroundColor: colorPalette.util[400],
-    },
-  },
-});
+// const expectedResponseCommon = {
+//   padding: '1rem',
+//   borderRadius: '0.5rem',
+//   backgroundColor: colorPalette.util[300],
+// };
+// export const addButton = style({
+//   ...expectedResponseCommon,
+//   cursor: 'pointer',
+//   transition: 'all 0.25s ease-in-out',
+//   selectors: {
+//     '&:hover': {
+//       backgroundColor: colorPalette.util[400],
+//     },
+//   },
+// });
 
-export const expectedResponseBox = style({
-  ...expectedResponseCommon,
-  height: '8rem',
-});
+// export const expectedResponseBox = style({
+//   ...expectedResponseCommon,
+//   height: '8rem',
+// });
--- frontend/src/pages/APIDocs/components/API/subComponents/APIBody/TestCase/ExpectedResponse/ExpectedResponse.tsx
+++ frontend/src/pages/APIDocs/components/API/subComponents/APIBody/TestCase/ExpectedResponse/ExpectedResponse.tsx
@@ -1,99 +1,99 @@
-import { createContext, ReactNode, useContext } from 'react';
-import Typography from '../../../../../../../../components/Typography';
-import * as s from './ExpectedResponse.css';
-import Flexbox from '../../../../../../../../components/Flexbox';
-import { HiPlusCircle } from 'react-icons/hi2';
-import Icon from '../../../../../../../../components/Icon';
-import JsonEditor from '../../../APICommon/JsonEditor/JsonEditor';
+// import { createContext, ReactNode, useContext } from 'react';
+// import Typography from '../../../../../../../../components/Typography';
+// import * as s from './ExpectedResponse.css';
+// import Flexbox from '../../../../../../../../components/Flexbox';
+// import { HiPlusCircle } from 'react-icons/hi2';
+// import Icon from '../../../../../../../../components/Icon';
+// import JsonEditor from '../../../APICommon/JsonEditor/JsonEditor';
 
-interface ExpectedResponseProps {
-  isEditing: boolean;
-  children: ReactNode;
-}
+// interface ExpectedResponseProps {
+//   isEditing: boolean;
+//   children: ReactNode;
+// }
 
-const ExpectedResponseContext = createContext<ExpectedResponseProps | null>(
-  null,
-);
+// const ExpectedResponseContext = createContext<ExpectedResponseProps | null>(
+//   null,
+// );
 
-const useExpectedResponseContext = () => {
-  const context = useContext(ExpectedResponseContext);
-  if (!context) {
-    throw new Error('');
-  }
-  return context;
-};
+// const useExpectedResponseContext = () => {
+//   const context = useContext(ExpectedResponseContext);
+//   if (!context) {
+//     throw new Error('');
+//   }
+//   return context;
+// };
 
-export const ExpectedResponse = ({
-  children,
-  isEditing,
-}: ExpectedResponseProps) => {
-  return (
-    <ExpectedResponseContext.Provider
-      value={{
-        children,
-        isEditing,
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '1.5rem',
-          textAlign: 'left',
-        }}
-      >
-        {children}
-      </div>
-    </ExpectedResponseContext.Provider>
-  );
-};
+// export const ExpectedResponse = ({
+//   children,
+//   isEditing,
+// }: ExpectedResponseProps) => {
+//   return (
+//     <ExpectedResponseContext.Provider
+//       value={{
+//         children,
+//         isEditing,
+//       }}
+//     >
+//       <div
+//         style={{
+//           display: 'flex',
+//           flexDirection: 'column',
+//           gap: '1.5rem',
+//           textAlign: 'left',
+//         }}
+//       >
+//         {children}
+//       </div>
+//     </ExpectedResponseContext.Provider>
+//   );
+// };
 
-ExpectedResponse.Schema = function Schema() {
-  const context = useExpectedResponseContext();
+// ExpectedResponse.Schema = function Schema() {
+//   const context = useExpectedResponseContext();
 
-  return (
-    <Flexbox flexDirections="col" style={s.containerStyle}>
-      <Typography color="secondary">Expected Response Schema</Typography>
+//   return (
+//     <Flexbox flexDirections="col" style={s.containerStyle}>
+//       <Typography color="secondary">Expected Response Schema</Typography>
 
-      {context.isEditing ? (
-        <JsonEditor isEditing={context.isEditing} />
-      ) : (
-        <ExpectedResponseAddButton />
-      )}
-    </Flexbox>
-  );
-};
+//       {context.isEditing ? (
+//         <JsonEditor isEditing={context.isEditing} />
+//       ) : (
+//         <ExpectedResponseAddButton />
+//       )}
+//     </Flexbox>
+//   );
+// };
 
-ExpectedResponse.Example = function Example() {
-  const context = useExpectedResponseContext();
-  return (
-    <Flexbox flexDirections="col" style={s.containerStyle}>
-      <Typography color="secondary">Expected Response Example</Typography>
+// ExpectedResponse.Example = function Example() {
+//   const context = useExpectedResponseContext();
+//   return (
+//     <Flexbox flexDirections="col" style={s.containerStyle}>
+//       <Typography color="secondary">Expected Response Example</Typography>
 
-      <JsonEditor jsonData={initial_json_dummy} isEditing={context.isEditing} />
-    </Flexbox>
-  );
-};
+//       {/* <JsonEditor jsonData={initial_json_dummy} isEditing={context.isEditing} /> */}
+//     </Flexbox>
+//   );
+// };
 
-// isEditing + 아직 등록안된경우 애드버튼 눌러서 등록해줘야함.
-const ExpectedResponseAddButton = (): ReactNode => (
-  <div className={s.addButton}>
-    <Icon background="none" color="light" size={3.5}>
-      <HiPlusCircle />
-    </Icon>
-  </div>
-);
+// // isEditing + 아직 등록안된경우 애드버튼 눌러서 등록해줘야함.
+// const ExpectedResponseAddButton = (): ReactNode => (
+//   <div className={s.addButton}>
+//     <Icon background="none" color="light" size={3.5}>
+//       <HiPlusCircle />
+//     </Icon>
+//   </div>
+// );
 
-const initial_json_dummy = {
-  status: 'success',
-  data: {
-    users: [
-      { id: 1, name: 'Alice' },
-      { id: 2, name: 'Bob' },
-    ],
-  },
-  meta: {
-    total: 2,
-    page: 1,
-  },
-};
+// const initial_json_dummy = {
+//   status: 'success',
+//   data: {
+//     users: [
+//       { id: 1, name: 'Alice' },
+//       { id: 2, name: 'Bob' },
+//     ],
+//   },
+//   meta: {
+//     total: 2,
+//     page: 1,
+//   },
+// };
