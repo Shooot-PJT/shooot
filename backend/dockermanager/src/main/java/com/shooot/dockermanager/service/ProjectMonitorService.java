572489b0d28378e92a1278bf6789e49fb2cd2196
박민희 <minuteman0212@naver.com>
2024-11-16 18:28:25 +0900

    be/fix: 부하 테스트 호출 명령어 수정

--- backend/dockermanager/src/main/java/com/shooot/dockermanager/service/ProjectMonitorService.java
+++ backend/dockermanager/src/main/java/com/shooot/dockermanager/service/ProjectMonitorService.java
@@ -5,9 +5,9 @@ import com.shooot.dockermanager.handler.ProjectDirectoryManager;
 import com.shooot.dockermanager.publisher.ProjectMonitorMessage;
 import com.shooot.dockermanager.publisher.ProjectMonitorMessagePublisher;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,16 +17,45 @@ public class ProjectMonitorService {
 
     private final ProjectDirectoryManager projectDirectoryManager;
     private final ProjectMonitorMessagePublisher projectMonitorMessagePublisher;
+    private final Map<String, String> keys = Map.of(
+        "instance1",
+        "/home/hyunjinkim/deployment/scripts/.vagrant/machines/instance1/virtualbox/private_key",
+        "instance2",
+        "/home/hyunjinkim/deployment/scripts/.vagrant/machines/instance2/virtualbox/private_key",
+        "instance3",
+        "/home/hyunjinkim/deployment/scripts/.vagrant/machines/instance3/virtualbox/private_key",
+        "instance4",
+        "/home/hyunjinkim/deployment/scripts/.vagrant/machines/instance4/virtualbox/private_key",
+        "instance5",
+        "/home/hyunjinkim/deployment/scripts/.vagrant/machines/instance5/virtualbox/private_key"
+    );
+    private final Map<String, Integer> ports = Map.of(
+        "instance1",
+        2222,
+        "instance2",
+        2200,
+        "instance3",
+        2201,
+        "instance4",
+        2202,
+        "instance5",
+        2203
+    );
+    private final String command = "sar -t 1";
 
     public void getStatus(Integer projectId, Integer projectJarFileId, Integer duration) {
         MetaData metaData = projectDirectoryManager.getMetaData(
             projectDirectoryManager.file(projectId, projectJarFileId).toPath());
-        System.out.println(metaData.getInstanceName());
 
         try {
-            ProcessBuilder processBuilder = new ProcessBuilder("/opt/vagrant/bin/vagrant", "ssh",
-                metaData.getInstanceName(), "-c", "ls").directory(
-                new File("/home/hyunjinkim/deployment/scripts"));
+            ProcessBuilder processBuilder = new ProcessBuilder(
+                "ssh",
+                "-o", "StrictHostKeyChecking=no",
+                "-i", keys.get(metaData.getInstanceName()),
+                "-p", ports.get(metaData.getInstanceName()).toString(),
+                "vagrant@localhost",
+                "'%s'".formatted(command)
+            );
             processBuilder.redirectErrorStream(true);
 
             Process process = processBuilder.start();
