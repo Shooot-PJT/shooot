8306282046064d2a8021142a58c72e665c341702
yoha6865 <yoha6865@gmail.com>
2024-11-16 23:28:18 +0900

    fe/fix: 빌드 에러 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom';
 import { APIDocs } from './pages/APIDocs';
 import { Signup } from './pages/Signup';
 import { Login } from './pages/Login';
-import { Tmp } from './pages/Tmp';
 import darkTheme from './styles/darkTheme.css';
 import { ServerTest } from './pages/ServerTest';
 import { Main } from './pages/Main';
@@ -14,7 +13,6 @@ function App() {
   return (
     <div className={darkTheme}>
       <Routes>
-        <Route path="/temp" element={<Tmp />} />
         <Route path="/auth">
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<Signup />} />
