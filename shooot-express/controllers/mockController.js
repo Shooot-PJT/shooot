ea25bb9a07263b48c21b770afc982d42af3da4c0
yoha6865 <yoha6865@gmail.com>
2024-11-16 22:44:09 +0900

    mock/feat: 데이터와 비교
    사용자 request 와 데이터 비교 후 적합한 test case 추출
    
    #74

--- shooot-express/controllers/mockController.js
+++ shooot-express/controllers/mockController.js
@@ -15,24 +15,79 @@ const getApiId = (method, endpoint) => {
     });
 };
 
-const getApiTestCaseIds = (apiId) => {
+const getApiTestCases = (apiId) => {
     return new Promise((resolve, reject) => {
-        const query = `SELECT api_test_case_id FROM api_test_case WHERE api_id = "${apiId}"`;
+        const query = `SELECT api_test_case_id, http_status_code FROM api_test_case WHERE api_id = "${apiId}"`;
 
         db.execute(query, (err, results) => {
             if (err) {
-                console.error("[post, /mock/data, getApiTestCaseIds]:", err);
+                console.error("[post, /mock/data, getApiTestCases]:", err);
                 return reject("Database query error");
             }
 
-            resolve(results.map((result) => result.api_test_case_id));
+            resolve(
+                results.map((result) => ({
+                    api_test_case_id: result.api_test_case_id,
+                    http_status_code: result.http_status_code,
+                }))
+            );
         });
     });
 };
 
-const getRequestContent = (testCaseId) => {
+const checkKeys = (src, target, keyword) => {
+    console.log("[CheckKeys] keyword:", keyword);
+    console.log("[CheckKeys] src:", JSON.stringify(src, null, 2));
+    console.log("[CheckKeys] target:", JSON.stringify(target, null, 2));
+    if (!src) {
+        if (!target) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        if (!target) {
+            return false;
+        } else {
+            const srcKeys = Object.keys(src).sort();
+            const targetKeys = Object.keys(target).sort();
+
+            if (srcKeys.length !== targetKeys.length) {
+                return false;
+            } else {
+                let result = true;
+                srcKeys.forEach((k, i) => {
+                    if (k !== targetKeys[i]) result = false;
+                });
+                return result;
+            }
+        }
+    }
+};
+
+const checkHeaders = (src, target) => {
+    console.log("[CheckHeaders] keyword: headers");
+    console.log("[CheckHeaders] src:", JSON.stringify(src, null, 2));
+    console.log("[CheckHeaders] target:", JSON.stringify(target, null, 2));
+    if (!target) {
+        return true;
+    } else {
+        const srcKeys = Object.keys(src);
+        const targetKeys = Object.keys(target);
+
+        let result = true;
+        targetKeys.forEach((k) => {
+            if (!(k in srcKeys)) {
+                result = false;
+            }
+        });
+        return result;
+    }
+};
+
+const getRequestContent = (testCase, src) => {
     return new Promise((resolve, reject) => {
-        const query = `SELECT * FROM api_test_case_request WHERE api_test_case_id = "${testCaseId}" ORDER BY api_test_request_id DESC LIMIT 1`;
+        const query = `SELECT * FROM api_test_case_request WHERE api_test_case_id = "${testCase.api_test_case_id}" ORDER BY api_test_request_id DESC LIMIT 1`;
 
         db.execute(query, (err, results) => {
             if (err) {
@@ -48,9 +103,25 @@ const getRequestContent = (testCaseId) => {
                     body: results[0].content.body,
                 },
             };
-            console.log("[Response] target:", target);
 
-            resolve(target);
+            const paramsResult = checkKeys(src.content.params, target.content.params, "params");
+            console.log(`[ParamsResult] ${paramsResult}\n`);
+            const variablesResult = checkKeys(src.content.pathvariable, target.content.pathvariable, "variables");
+            console.log(`[VariablesResult] ${variablesResult}\n`);
+            const headersResult = checkHeaders(src.content.headers, target.content.headers, "headers");
+            console.log(`[HeadersResult] ${headersResult}\n`);
+            const formDataResult = checkKeys(src.content.body.formData, target.content.body.formData, "formData");
+            console.log(`[FormDataResult] ${formDataResult}\n`);
+            const rawResult = checkKeys(src.content.body.raw, target.content.body.raw, "raw");
+            console.log(`[RawResult] ${rawResult}\n`);
+
+            if (paramsResult && variablesResult && headersResult && formDataResult && rawResult) {
+                console.log("[Response] target:", results[0]);
+                resolve({
+                    httpStatusCode: testCase.http_status_code,
+                    exampleResponse: results[0].content.expectedResponse.example,
+                });
+            }
         });
     });
 };
@@ -66,23 +137,33 @@ exports.mockData = async (req, res) => {
                 body: body,
             },
         };
+        if (src.content.body.formData.datas === null && src.content.body.formData.files === null)
+            src.content.body.formData = null;
+        if (src.content.body.raw.datas === null && src.content.body.raw.files === null) src.content.body.raw = null;
+
         console.log("[Request] endpoint:", endpoint);
         console.log("[Request] method:", method);
         console.log("[Request] headers:", headers);
         console.log("[Request] requestParameters:", requestParameters);
         console.log("[Request] pathVariables:", pathVariables);
         console.log("[Request] body:", body);
+        console.log("[Request] formData:", body.formData);
+        console.log("[Request] raw:", body.raw);
         console.log("[Request] src:", src);
 
         const apiId = await getApiId(method, endpoint);
         if (!apiId) return res.status(404).json({ message: "등록된 api 가 없습니다" });
 
-        const testCaseIds = await getApiTestCaseIds(apiId);
-        if (testCaseIds.length === 0) return res.status(404).json({ message: "등록된 api test case 가 없습니다" });
+        const testCases = await getApiTestCases(apiId);
+        if (testCases.length === 0) return res.status(404).json({ message: "등록된 api test case 가 없습니다" });
 
-        const results = await Promise.all(testCaseIds.map((id) => getRequestContent(id)));
-        const response = { results: results.map((content) => ({ content })) };
-        return res.status(200).json(response);
+        const results = await Promise.all(testCases.map((val) => getRequestContent(val, src)));
+        if (!results.length) {
+            return res.status(404).json({ message: "요청에 맞는 api test case 가 없습니다" });
+        } else {
+            console.log("[Response] results:", results);
+            return res.status(200).json(results[0]);
+        }
     } catch (error) {
         return res.status(500).json({ error: "Database query error" });
     }
