719fabfd2ed1c382f65b07518f76ca583609ed13
장철현 <wkdcjfgus000@naver.com>
2024-11-16 18:41:39 +0900

    be/fix : TestCase Response 데이터 추가

--- backend/shooot/src/main/java/com/shooot/application/api/ui/dto/TestCaseView.java
+++ backend/shooot/src/main/java/com/shooot/application/api/ui/dto/TestCaseView.java
@@ -4,6 +4,7 @@ package com.shooot.application.api.ui.dto;
 import com.shooot.application.api.domain.ApiTestStatusType;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @Builder
@@ -14,6 +15,11 @@ import java.util.Map;
 public class TestCaseView {
     private Integer id;
     private Integer testCaseRequestId;
+    private String title;
+    private String testStatus;
+    private Integer httpStatusCode;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
     private String type;
     private Map<String, Object> content;
 
