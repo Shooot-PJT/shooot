ab297c9795fd05801481c05ce50376b81417ab9c
박민희 <minuteman0212@naver.com>
2024-11-16 16:16:17 +0900

    be/chore: ProcessBuilder directory 메소드 동작 확인

--- backend/dockermanager/src/main/java/com/shooot/dockermanager/service/ProjectMonitorService.java
+++ backend/dockermanager/src/main/java/com/shooot/dockermanager/service/ProjectMonitorService.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +25,11 @@ public class ProjectMonitorService {
         System.out.println(metaData.getInstanceName());
 
         try {
+            ProcessBuilder processBuilderTest = new ProcessBuilder("ls").directory(
+                new File("/home/hyunjinkim/deployment/scripts"));
+            System.out.println(
+                Arrays.toString(processBuilderTest.start().getInputStream().readAllBytes()));
+
             ProcessBuilder processBuilder = new ProcessBuilder("vagrant", "ssh",
                 metaData.getInstanceName(), "-c", "'sar -t 1'").directory(
                 new File("/home/hyunjinkim/deployment/scripts"));
