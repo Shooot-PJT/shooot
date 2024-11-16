9b7464e03e182912094ccdae202c8745d21c7339
박민희 <minuteman0212@naver.com>
2024-11-16 15:12:11 +0900

    be/feat: 부하 테스트 정보 Redis Stream 활용하여 전송하도록 변경
    #72

--- backend/dockermanager/src/main/java/com/shooot/dockermanager/service/ProjectMonitorService.java
+++ backend/dockermanager/src/main/java/com/shooot/dockermanager/service/ProjectMonitorService.java
@@ -3,7 +3,12 @@ package com.shooot.dockermanager.service;
 import com.shooot.dockermanager.handler.MetaData;
 import com.shooot.dockermanager.handler.ProjectDirectoryManager;
 import com.shooot.dockermanager.handler.ProjectDirectoryManager.DirStructure;
+import com.shooot.dockermanager.publisher.ProjectMonitorMessage;
+import com.shooot.dockermanager.publisher.ProjectMonitorMessagePublisher;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +17,40 @@ import org.springframework.stereotype.Service;
 public class ProjectMonitorService {
 
     private final ProjectDirectoryManager projectDirectoryManager;
+    private final ProjectMonitorMessagePublisher projectMonitorMessagePublisher;
 
-    public String getStatus(Integer projectId, Integer projectJarFileId, Integer duration) {
+    public void getStatus(Integer projectId, Integer projectJarFileId, Integer duration) {
         File metaDataFile = projectDirectoryManager.getFile(projectId, projectJarFileId,
             DirStructure.METADATA).orElseThrow();
         MetaData metaData = projectDirectoryManager.getMetaData(metaDataFile.toPath());
-        new ProcessBuilder("vagrant", "ssh", metaData.getInstanceName(), "-c", "'sar -t 1'");
 
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("vagrant", "ssh",
+                metaData.getInstanceName(), "-c", "'sar -t 1'");
+            processBuilder.redirectErrorStream(true);
+
+            Process process = processBuilder.start();
+
+            long currentTime = System.currentTimeMillis();
+            long endTime = currentTime + duration * 1000;
+
+            try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while (System.currentTimeMillis() < endTime) {
+                    line = reader.readLine();
+                    projectMonitorMessagePublisher.publish(
+                        ProjectMonitorMessage.builder()
+                            .projectId(projectId)
+                            .projectJarFileId(projectJarFileId)
+                            .message(line)
+                            .build());
+                }
+            }
+
+            process.destroy();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
