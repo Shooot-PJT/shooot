4502e924e948719fbe7ebb8e91c3afc2a97d9c8f
yoha6865 <yoha6865@gmail.com>
2024-11-17 04:48:46 +0900

    mock/feat: service worker 동작 방식 전면 수정
    #74

--- shooot-mock/src/index.d.ts
+++ shooot-mock/src/index.d.ts
@@ -2,42 +2,29 @@ import { AxiosRequestConfig, AxiosStatic, AxiosResponse } from "axios";
 
 export interface Configs {
     projectName: string;
-    delay?: number;
 }
 
 export interface Shooot extends Configs {
     axios: Omit<AxiosStatic, "get" | "post" | "put" | "patch" | "delete"> & {
-        get<T = any, R = AxiosResponse<T>, D = any>(
-            url: string,
-            config?: AxiosRequestConfig<D>,
-            pathVariables?: object
-        ): Promise<R>;
+        get<T = any, R = AxiosResponse<T>, D = any>(url: string, config?: AxiosRequestConfig<D>): Promise<R>;
         post<T = any, R = AxiosResponse<T>, D = any>(
             url: string,
             data?: any,
-            config?: AxiosRequestConfig<D>,
-            pathVariables?: object
+            config?: AxiosRequestConfig<D>
         ): Promise<R>;
         put<T = any, R = AxiosResponse<T>, D = any>(
             url: string,
             data?: any,
-            config?: AxiosRequestConfig<D>,
-            pathVariables?: object
+            config?: AxiosRequestConfig<D>
         ): Promise<R>;
         patch<T = any, R = AxiosResponse<T>, D = any>(
             url: string,
             data?: any,
-            config?: AxiosRequestConfig<D>,
-            pathVariables?: object
+            config?: AxiosRequestConfig<D>
         ): Promise<R>;
-        delete<T = any, R = AxiosResponse<T>, D = any>(
-            url: string,
-            config?: AxiosRequestConfig<D>,
-            pathVariables?: object
-        ): Promise<R>;
+        delete<T = any, R = AxiosResponse<T>, D = any>(url: string, config?: AxiosRequestConfig<D>): Promise<R>;
     };
     setConfigs: (projectName: string, delay?: number) => Promise<void>;
-    controller: (mode?: string) => Promise<void>;
     register: () => Promise<void>;
     unregister: () => Promise<void>;
 }
