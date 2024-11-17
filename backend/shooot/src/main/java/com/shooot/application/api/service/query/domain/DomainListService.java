90444178cb094373890f18fd57ed14d4c64c10e4
장철현 <wkdcjfgus000@naver.com>
2024-11-17 21:27:57 +0900

    be/fix : 도메인 구독 상태 response에 추가

--- backend/shooot/src/main/java/com/shooot/application/api/service/query/domain/DomainListService.java
+++ backend/shooot/src/main/java/com/shooot/application/api/service/query/domain/DomainListService.java
@@ -2,6 +2,7 @@ package com.shooot.application.api.service.query.domain;
 
 import com.shooot.application.api.domain.Domain;
 import com.shooot.application.api.domain.repository.DomainRepository;
+import com.shooot.application.api.domain.repository.DomainSubscribeRepository;
 import com.shooot.application.api.ui.dto.DomainView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,18 +14,36 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class DomainListService {
     private final DomainRepository domainRepository;
+    private final DomainSubscribeRepository domainSubscribeRepository;
 
-    public List<DomainView> getDomainList(Integer projectId){
+    public List<DomainView> getDomainList(Integer projectId, Integer userId){
         List<Domain> domains = domainRepository.findByProjectIdAndNotDeleted(projectId);
 
         return domains.stream()
-                .map(domain -> DomainView.builder()
-                        .domainId(domain.getId())
-                        .projectId(domain.getProject().getId())
-                        .title(domain.getName())
-                        .description(domain.getDescription())
-                        .build()
-                ).collect(Collectors.toList());
+                .map(domain -> {
+                    boolean isSubscribed = domainSubscribeRepository.findDomainSubscribeByUserIdAndDomainId(userId, domain.getId())
+                            .isPresent();
+
+                    return DomainView.builder()
+                            .domainId(domain.getId())
+                            .projectId(domain.getProject().getId())
+                            .title(domain.getName())
+                            .description(domain.getDescription())
+                            .isSubscribe(isSubscribed)
+                            .build();
+
+                }).toList();
+
+
+
+//        return domains.stream()
+//                .map(domain -> DomainView.builder()
+//                        .domainId(domain.getId())
+//                        .projectId(domain.getProject().getId())
+//                        .title(domain.getName())
+//                        .description(domain.getDescription())
+//                        .build()
+//                ).collect(Collectors.toList());
     }
 
 
