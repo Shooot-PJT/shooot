01d7d980df56cac342dc272047f226a8b858e020
박민희 <minuteman0212@naver.com>
2024-11-17 19:31:22 +0900

    be/feat: 부하 테스트 결과 기록 위한 테이블 생성
    #95

--- backend/shooot/src/main/java/com/shooot/application/projecttest/domain/StressTestResult.java
+++ backend/shooot/src/main/java/com/shooot/application/projecttest/domain/StressTestResult.java
@@ -32,28 +32,55 @@ public class StressTestResult extends SoftDeleteEntity {
     @JoinColumn(name = "stress_test_log_id")
     private StressTestLog stressTestLog;
 
+    @Column(name = "http_method")
     private String httpMethod;
+
+    @Column(name = "url")
     private String url;
 
+    @Column(name = "duration")
     private Integer duration;
+
+    @Column(name = "vuser")
     private Integer vUser;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "test_method")
     private BuildFileTestMethod testMethod;
 
+    @Column(name = "avg_cpu")
     private Double avgCpu;
+
+    @Column(name = "max_cpu")
     private Double maxCpu;
+
+    @Column(name = "min_cpu")
     private Double minCpu;
 
+    @Column(name = "avg_memory")
     private Double avgMemory;
+
+    @Column(name = "max_memory")
     private Double maxMemory;
+
+    @Column(name = "min_memory")
     private Double minMemory;
 
+    @Column(name = "avg_disk")
     private Double avgDisk;
+
+    @Column(name = "max_disk")
     private Double maxDisk;
+
+    @Column(name = "min_disk")
     private Double minDisk;
 
+    @Column(name = "avg_network")
     private Double avgNetwork;
+
+    @Column(name = "max_network")
     private Double maxNetwork;
+
+    @Column(name = "min_network")
     private Double minNetwork;
 }
