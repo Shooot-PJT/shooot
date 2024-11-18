78aa63721844191888190b77fae1dd415d83c4ae
최요하 <yoha6865@gmail.com>
2024-11-18 23:54:58 +0900

    fe/fix: 공용 로그인 info 전송 방식 수정
    #87

--- frontend/src/pages/APIDocs/apis/commonLogin/index.ts
+++ frontend/src/pages/APIDocs/apis/commonLogin/index.ts
@@ -1,6 +1,9 @@
 import { api } from '../../../../apis/interceptors';
 import { CommonLoginResponse } from './types';
 
-export const commonLogin = (infos: Record<string, string>) => {
-  return api.post<CommonLoginResponse>('', infos);
+export const commonLogin = (endpoint: string, infos: string) => {
+  return api.post<CommonLoginResponse>('', {
+    endpoint: endpoint,
+    info: infos,
+  });
 };
